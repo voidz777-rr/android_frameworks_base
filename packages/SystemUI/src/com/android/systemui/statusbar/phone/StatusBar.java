a96d53e25a2370fbf815ee43035db505a518a013
lion0738 <lion0738@naver.com>
2016-01-25 17:07:14 +0900

    Don't update if HeadsUp is disabled
    Heads up is still showing even if it's disabled.
    
    Change-Id: Ib9c39d9e1d566ab14c0201e4e89f2b55f008422f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -8892,7 +8892,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         boolean shouldPeek = shouldPeek(entry, notification);
         boolean alertAgain = alertAgain(entry, n);
 
-        updateHeadsUp(key, entry, shouldPeek, alertAgain);
+        if (mUseHeadsUp) {
+           updateHeadsUp(key, entry, shouldPeek, alertAgain);
+        }
         updateNotifications();
 
         if (!notification.isClearable()) {
