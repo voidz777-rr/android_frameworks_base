af9ff6551a2e9b58b697e15a2fe8b1340b8e2b55
Alex <alex@dirtyunicorns.com>
2018-06-11 17:03:25 +0200

    Ensure the clockview is aligned correctly each time
    Previously if you switched from Analog Clock to any of the Sammy style
    clock views, you would notice sometimes that the view wasn't aligned
    correctly. Shutting the device off and turning it back on would fix this
    issue but that's just not the right fix for this.
    
    Change-Id: Iae3d8fd875bef11f485c8ec8167861bb63fdef55

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -47,6 +47,7 @@ import android.text.Html;
 import android.text.TextUtils;
 import android.text.format.DateFormat;
 import android.util.AttributeSet;
+import android.view.Gravity;
 import android.util.Log;
 import android.util.Slog;
 import android.util.TypedValue;
@@ -896,12 +897,14 @@ public class KeyguardStatusView extends GridLayout implements
             default:
                 params.addRule(RelativeLayout.BELOW, R.id.clock_view);
                 mClockView.setSingleLine(true);
+		mClockView.setGravity(Gravity.CENTER);
 		mAnalogClockView.unregisterReceiver();
                 mDeadPoolClockView.unregisterReceiver();
                 break;
             case 1: // digital (bold)
                 params.addRule(RelativeLayout.BELOW, R.id.clock_view);
                 mClockView.setSingleLine(true);
+		mClockView.setGravity(Gravity.CENTER);
 		mAnalogClockView.unregisterReceiver();
                 mDeadPoolClockView.unregisterReceiver();
                 break;
@@ -918,12 +921,14 @@ public class KeyguardStatusView extends GridLayout implements
             case 4: // sammy
                 params.addRule(RelativeLayout.BELOW, R.id.clock_view);
                 mClockView.setSingleLine(false);
+		mClockView.setGravity(Gravity.CENTER);
 		mAnalogClockView.unregisterReceiver();
                 mDeadPoolClockView.unregisterReceiver();
                 break;
             case 5: // sammy (bold)
                 params.addRule(RelativeLayout.BELOW, R.id.clock_view);
                 mClockView.setSingleLine(false);
+		mClockView.setGravity(Gravity.CENTER);
 		mAnalogClockView.unregisterReceiver();
                 mDeadPoolClockView.unregisterReceiver();
                 break;
